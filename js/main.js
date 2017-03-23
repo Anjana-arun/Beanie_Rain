d7fb6d81881daf6ed95b2358a4fbc0232ea71982
Anjana <anjoo.pilai@gmail.com>
2017-03-23 23:47:11 +0530

    Create clouds in sky

--- js/main.js
+++ js/main.js
@@ -2,17 +2,24 @@
 
   var canvas = document.querySelector('#game_main');
   var ctx = canvas.getContext('2d');
-  ctx.canvas.width=window.innerWidth/1.019;
-   ctx.canvas.height=window.innerHeight/1.015;
+  ctx.canvas.width=0.9*window.innerWidth;
+   ctx.canvas.height=0.8*window.innerHeight;
   // BG Canvas
+
+ 
   var bg_canvas = document.querySelector('#game_bg');
   var bg_ctx = bg_canvas.getContext('2d');
 
-  bg_canvas.width = 0.9*canvas.width;
-  bg_canvas.height = 0.9*canvas.height;
-
-
-  // Game play on mouse clicks too!
+  bg_canvas.width = window.innerWidth;
+  bg_canvas.height = window.innerHeight;
+ 
+  for(i=0;i<10;i++){
+     var x=(Math.random()*ctx.canvas.width)+1;
+    var y=(Math.random()*ctx.canvas.height)+1;
+    
+    $cloudgen.drawCloud(ctx,x,y,80,{r:255,g:255,b:255},0.9);
+}
+ // Game play on mouse clicks too!
   window.addEventListener('mousedown', function(e) {
     mit.ascend();
   }, false);
